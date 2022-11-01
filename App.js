61830a14f9381f56c000e32d1ff4915e02e1952c
irfan <engr.irfandayan@gmail.com>
2022-11-01 12:35:21 +0500

    Add react native guess number app

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess an Number" />
+      <StartGameScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
---
+++ screens/GameScreen.js
@@ -0,0 +1,7 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const GamerScreen = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default GamerScreen;
